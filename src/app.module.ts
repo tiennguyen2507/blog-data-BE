6e158a9b90ec18f982f944f9c6651ffef8e39731
tien nguyen <nguyenledinhtien2507@gmail.com>
2024-04-29 09:05:27 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { DatabaseModule } from './database/database.module';
+// import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
