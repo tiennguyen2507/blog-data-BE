5b3f34b64fe76c537722ea13b1279d6be8fd7f6a
tien nguyen <nguyenledinhtien2507@gmail.com>
2024-04-29 08:57:26 +0700

    add log and swapger

---
+++ src/database/database.providers.ts
@@ -0,0 +1,18 @@
+import * as mongoose from 'mongoose';
+import * as log from 'log-beautify';
+
+export const databaseProviders = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: (): Promise<typeof mongoose> => {
+      return mongoose
+        .connect(
+          'mongodb+srv://tiennguyen2507:tien123@cluster0.7pasb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+        )
+        .then((value) => {
+          log.success_('Connect DB success!!');
+          return value;
+        });
+    },
+  },
+];
